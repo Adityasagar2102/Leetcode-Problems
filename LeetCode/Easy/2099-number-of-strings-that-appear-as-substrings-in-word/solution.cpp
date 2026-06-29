f8c9d61f43d5e0bc5fff349b6988124f92121920
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-06-30 00:15:55 +0530

    [LC] Sync: number-of-strings-that-appear-as-substrings-in-word (C++) Runtime 0 ms

--- LeetCode/Easy/2099-number-of-strings-that-appear-as-substrings-in-word/solution.cpp
+++ LeetCode/Easy/2099-number-of-strings-that-appear-as-substrings-in-word/solution.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
+        int n = patterns.size();
         int cnt = 0;
-
-        for (string &temp : patterns) {
-            bool found = false;
-
-            for (int j = 0; j < word.size(); j++) {
-                int a = 0;
-                int b = j;
-
-                while (a < temp.size() && b < word.size() &&
-                       temp[a] == word[b]) {
+        
+        for(int i=0; i<n; i++){
+            string temp = patterns[i];
+            bool match = false;
+            for(int j=0; j<word.size(); j++){
+                int a =0;
+                int b=j;
+                while(a<temp.size() && b<word.size() && temp[a] == word[b]){
                     a++;
                     b++;
                 }
-
-                if (a == temp.size()) {
-                    found = true;
+                if(a==temp.size()){
+                    match = true;
                     break;
                 }
             }
-
-            if (found) cnt++;
+            if(match) cnt++;
         }
-
         return cnt;
     }
 };
