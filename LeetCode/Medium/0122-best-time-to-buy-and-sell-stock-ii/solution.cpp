4ad6a11e52c501cb3b66bb571a76e7f73ce1bb66
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-06-30 16:10:09 +0530

    [LC] Sync: best-time-to-buy-and-sell-stock-ii (C++) Runtime 2 ms

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int profit = 0;
+    vector<vector<int>>dp;
+    int solve(vector<int>& prices, bool buy, int ind){
+        if(ind == prices.size()) return 0;
+        int stockbuy = 0;
+        int stocksell = 0;
+        if(dp[buy][ind]!= -1) return dp[buy][ind];
 
-        for(int i=0; i<n-1; i++){
-            if(prices[i+1]> prices[i]) profit += (prices[i+1]-prices[i]);
+        if(buy){ // stock is bought
+            stockbuy = max(prices[ind] + solve(prices,false,ind+1),solve(prices,true,ind+1));
+        }else{
+            stocksell = max(solve(prices,true,ind+1) -prices[ind], solve(prices,false,ind+1));
         }
 
-        return profit;
+        return dp[buy][ind] = max(stocksell,stockbuy);
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        // int profit = 0;
+
+        // for(int i=0; i<n-1; i++){
+        //     if(prices[i+1]> prices[i]) profit += (prices[i+1]-prices[i]);
+        // }
+
+        // return profit;
+        dp.assign(3,vector<int>(n+1,-1));
+        return solve(prices,false,0);
     }
 };
