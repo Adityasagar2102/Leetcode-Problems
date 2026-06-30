003f002e019b2094ca82457fd62b37b7ae9b0187
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-06-30 16:46:59 +0530

    [LC] Sync: best-time-to-buy-and-sell-stock-with-cooldown (C++) Runtime 1 ms

---
+++ LeetCode/Medium/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int solve(vector<int>& prices, bool buy, int ind){
+        if(ind >= prices.size()) return 0;
+        int profit = 0;
+        if(dp[buy][ind]!= -1) return dp[buy][ind];
+
+        if(buy){ // stock is bought
+            profit = max(prices[ind] + solve(prices,0,ind+2),solve(prices,1,ind+1));
+        }else{
+            profit = max(solve(prices,1,ind+1) -prices[ind], solve(prices,0,ind+1));
+        }
+
+        return dp[buy][ind] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        dp.assign(3,vector<int>(n+1,-1));
+        return solve(prices,0,0);
+    }
+};
