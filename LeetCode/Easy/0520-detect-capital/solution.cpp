7219dddfcef5c6c7bc98cc9f64304d9dc977f64b
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-07-08 13:45:25 +0530

    [LC] Sync: detect-capital (C++) Runtime 0 ms

---
+++ LeetCode/Easy/0520-detect-capital/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.size();
+        int cnt = 0;
+        bool capital = true;
+        if(word[0]>='A' && word[0]<='Z'){
+            // cnt++;
+        }else capital = false;
+
+
+        for(int i=1; i<n; i++){
+            if(word[i] >='A' && word[i]<='Z'){
+                cnt++;
+                if(!capital) return false;
+            }
+        }
+
+        if(cnt==n-1 || cnt==0) return true;
+        return false;
+    }
+};
