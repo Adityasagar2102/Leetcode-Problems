fd5458cbf7a84e6565e795f2a79de9bcfcb1ae55
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-07-16 03:13:41 +0530

    [LC] Sync: gcd-of-odd-and-even-sums (C++) Runtime 0 ms

--- LeetCode/Easy/3995-gcd-of-odd-and-even-sums/solution.cpp
+++ LeetCode/Easy/3995-gcd-of-odd-and-even-sums/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int even = 0;
-        int odd = 0;
+        // int even = 0;
+        // int odd = 0;
 
-        for(int i=0; i<=n;i++){
-            even += i*2;
-            if(i!=n)odd += i*2+1;
-            // cout<<"even "<<even<<" odd "<<odd<<endl;
-        }
+        // for(int i=0; i<=n;i++){
+        //     even += i*2;
+        //     if(i!=n)odd += i*2+1;
+        //     // cout<<"even "<<even<<" odd "<<odd<<endl;
+        // }
 
-        return gcd(odd,even);
+        // return gcd(odd,even);
+
+        return n;
     }
 };
