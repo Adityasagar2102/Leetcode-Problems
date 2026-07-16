223132fe603de81fbcacb55b72c18659874142a5
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-07-16 15:30:41 +0530

    [LC] Sync: parsing-a-boolean-expression (C++) Runtime 3 ms

---
+++ LeetCode/Hard/1197-parsing-a-boolean-expression/solution.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool parse(string& ex, int &i){
+        
+        if(ex[i]=='t'){
+            i++;
+            return true;
+        }
+        if(ex[i]=='f'){
+            i++;
+            return false;
+        }
+        if(ex[i]=='!'){
+            i+=2;
+
+            bool temp = !parse(ex,i);
+            i++;
+
+            return temp;
+        }
+        if(ex[i]=='|'){
+            bool ans = false;
+            i+=2;
+
+            while(ex[i]!=')'){
+                bool value = parse(ex,i);
+                // if(value) return true;
+                ans = ans || value;
+
+                if(ex[i]==','){
+                    i++;
+                }
+            }
+            i++;
+            return ans;
+        }
+        if(ex[i]=='&'){
+            bool ans = true;
+            i+=2;
+
+            while(ex[i]!=')'){
+                bool value = parse(ex,i);
+                // if(!value) return false;
+                ans = ans && value;
+
+                if(ex[i]==',') i++;
+            }
+            i++;
+            return ans;
+        }
+        return false;
+    }
+    bool parseBoolExpr(string expression) {
+        int i=0;
+        return parse(expression,i);
+    }
+};
