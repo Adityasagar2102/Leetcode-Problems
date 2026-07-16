76d010379a806c9835313b9b57e4355f5ca2aff1
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-07-16 18:20:29 +0530

    [LC] Sync: partition-array-for-maximum-sum (C++) Runtime 5 ms

--- LeetCode/Medium/1121-partition-array-for-maximum-sum/solution.cpp
+++ LeetCode/Medium/1121-partition-array-for-maximum-sum/solution.cpp
@@ -6,9 +6,10 @@ public:
         if(dp[i]!=-1) return dp[i];
 
         int ans = INT_MIN;
+        int mx = INT_MIN;
         for(int j = 1; j<=k && i+j <= arr.size(); j++){
-            int sum = *max_element(arr.begin()+i, arr.begin()+i+j);
-            ans = max(ans, solve(i+j, arr, k) + sum *(j));
+            mx = max(mx,arr[i+j-1]);
+            ans = max(ans, solve(i+j, arr, k) + mx *(j));
         }
         return dp[i] = ans;
     }
