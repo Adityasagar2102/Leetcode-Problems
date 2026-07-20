86e2a9176fd68b3b2ad4b9aad2eb25ccfc6b5900
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-07-20 23:42:24 +0530

    [LC] Sync: majority-element (C++) Runtime 0 ms

--- LeetCode/Easy/0169-majority-element/solution.cpp
+++ LeetCode/Easy/0169-majority-element/solution.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int maj = nums[0];
+        int maj =0;
         int cnt = 0;
 
         for(int i=0; i<n; i++){
-            if(maj==nums[i]){
+            if(cnt==0){
+                maj = nums[i];
+            }
+            if(maj == nums[i]){
                 cnt++;
-            }else{
-                cnt--;
-                cout << cnt<< endl;
+                // cout << cnt<< endl;
             }
-            if(cnt<0){
-                cnt=0;
-                maj=nums[i];
-            }
+            else cnt--;
         }
 
         return maj;
