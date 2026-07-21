0c1797ca0ad7914e5233062669670066ad6f461c
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-07-21 23:23:08 +0530

    [LC] Sync: pascals-triangle (C++) Runtime 0 ms

---
+++ LeetCode/Easy/0118-pascals-triangle/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        int size = 1;
+
+        vector<vector<int>> ans(numRows);
+
+        for(int i=1; i<=numRows; i++){
+            vector<int>temp(i);
+            for(int j=0; j<i; j++){
+                if(j==0 || j==i-1){
+                    temp[j] = 1;
+                }
+                else{
+                    temp[j] = ans[i-2][j] + ans[i-2][j-1];
+                }
+            }
+            ans[i-1] = temp;
+        }
+
+        return ans;
+    }
+};
