83275940c73cbc0a060632aa047985f21306bc3a
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-07-22 12:36:28 +0530

    [LC] Sync: majority-element-ii (C++) Runtime 0 ms

---
+++ LeetCode/Medium/0229-majority-element-ii/solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int ele1 = -1,ele2 = -1;
+        int cnt1 = 0, cnt2 = 0;
+
+        int freq = nums.size()/3;
+
+        for(auto num: nums){
+            if(num == ele1) cnt1++;
+            else if(num == ele2) cnt2++;
+            else if(cnt1 == 0){
+                ele1 = num;
+                cnt1 = 1;
+            }else if(cnt2 == 0){
+                ele2 = num;
+                cnt2 = 1;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1=0;
+        cnt2=0;
+
+        for(auto num: nums){
+            if(num==ele1)cnt1++;
+            if(num==ele2)cnt2++;
+        }
+
+        vector<int> ans;
+        if(cnt1>freq) ans.push_back(ele1);
+        if(cnt2>freq) ans.push_back(ele2);
+
+        // cout<<ele1<< " "<<ele2;
+        if(ans.size()==0) return {};
+        if(ans.size()==2 && ans[0]!=ans[1])
+        return ans;
+        else return {ans[0]};
+    }
+};
