d78d7f22a4d634ce01525eb6e83d177d3683cd24
Adityasagar2102 <110262180+Adityasagar2102@users.noreply.github.com>
2026-07-24 17:25:14 +0530

    [LC] Sync: search-insert-position (C++) Runtime 0 ms

--- LeetCode/Easy/0035-search-insert-position/solution.cpp
+++ LeetCode/Easy/0035-search-insert-position/solution.cpp
@@ -8,9 +8,9 @@ public:
         while(low<=high){
             int mid = low + (high-low)/2;
 
-            if(nums[mid]==target) return mid;
-            if(nums[mid]<target) low = mid+1;
-            else high = mid-1;
+            // if(nums[mid]==target) return mid;
+            if(nums[mid]>=target) high = mid-1;
+            else low = mid+1;
         } 
 
         return low;
